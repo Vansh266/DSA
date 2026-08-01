24bfdff4adf630d13e23ccce5e70be3c500dae20
Vansh266 <vanshsagar266@gmail.com>
2026-08-01 16:14:21 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (53.49%) - LeetSync

---
+++ 551-student-attendance-record-i/student-attendance-record-i.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool checkRecord(string s) {
+        int a=0;
+        int l=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='A'){
+                a++;
+                l=0;
+            }
+            else if(s[i]=='L'){
+                l++;
+
+                if(l>=3){ 
+                    return false;
+                }
+            }
+            else{
+                l=0;
+            }
+            if(a>=2){
+                return false;
+        }
+    }
+    return true;
+    }
+};
